139a662ecd28ce76a09a2db2142304cc4b5f41f6
AJ Behncke <ajbinky@gmail.com>
2018-10-11 13:48:08 -0500

    initial

---
+++ Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Employee
+    {
+        public string firstName { get; set; }
+        public string middleName { get; set; }
+        public string lastName { get; set; }
+
+        public DateTime birthDate { get; set; }
+
+        public DateTime hireDate { get; set; }
+
+        public Enums.Department department { get; set; }
+
+        public String jobTitle { get; set; }
+
+        public double salary { get; set; }
+
+        public Enums.SalaryType salaryType { get; set; }
+
+        public int ID { get; set; }
+
+        public string availableHours { get; set; }
+    }
+}
